d7c0753ddef52276eb60ae8413e7bc341fdcf9a9
Abrahamthulei <thuleilea@gmail.com>
2025-04-17 20:31:20 +0300

    Add files via upload

---
+++ datad.sql
@@ -0,0 +1,15 @@
+-- 📌 Question 1 🗑️
+-- Drop an index named IdxPhone from customers table
+DROP INDEX IdxPhone ON customers;
+
+-- 📌 Question 2 👤
+-- Create a user named 'bob' with password 'S$cu3r3!' restricted to localhost
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- 📌 Question 3 🔑
+-- Grant INSERT privilege to user 'bob' on database 'salesDB'
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- 📌 Question 4 🔐
+-- Change the password for user 'bob' to 'P$55!23'
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
